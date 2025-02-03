052c7de6b355be2d82bb8a15fcae2d5b5725c4d8
Naga Sai <108686878+saijanipireddy@users.noreply.github.com>
2025-02-03 18:38:59 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,9 @@ const port = process.env.PORT || 4000
 // middleware 
 
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+    origin: "https://fullstack-app-admin.onrender.com/"
+}))
 app.use('/images', express.static(join(__dirname, 'uploads')));
 
 // DB Connection 
